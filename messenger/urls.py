f815526e513a2046c7edd9e284d787d49383a89b
Amr Saeed <amrsaeed@aucegypt.edu>
2017-05-27 19:58:43 +0200

    Removed IDE specific files

--- messenger/urls.py
+++ messenger/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'messenger'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^/detail', views.detail, name='detail')
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello, World!")
+    latest_message_list = Message.objects.order_by('-sent')[:5]
+    context = {'latest_message_list': latest_message_list}
+    return render(request, 'messenger/index.html', context)
+
+def detail(request, id):
+    return HttpResponse(id)
