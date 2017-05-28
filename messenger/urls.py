c7184efe63b696f6b3d8ac3e4f664c8be7170bfb
Amr Saeed <amrsaeed@aucegypt.edu>
2017-05-28 04:11:16 +0200

    Finished Login and Signup page

--- messenger/urls.py
+++ messenger/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'messenger'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/detail', views.detail, name='detail')
+    url(r'^webchat/$', views.webchat, name='webchat'),
+    url(r'^signin/$', views.signin, name='signin'),
+    url(r'^signup/$', views.signup, name='signup')
+
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,14 +1,66 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Message
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def index(request):
-    latest_message_list = Message.objects.order_by('-sent')[:5]
-    context = {'latest_message_list': latest_message_list}
+    logout(request)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('messenger:webchat'))
+
+    return render(request, 'messenger/index.html')
+
+
+def signin(request):
+    user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse('messenger:webchat'))
+    else:
+        context = {'error_message': 'Login Failed'}
+        return render(request, 'messenger/index.html', context)
+
+
+def signup(request):
+    first_name = request.POST.get('fname')
+    last_name = request.POST.get('lname')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    email = request.POST.get('email')
+
+    try:
+        u = User.objects.get(username=username)
+    except User.DoesNotExist:
+        u = None
+
+    try:
+        e = User.objects.get(email=email)
+    except User.DoesNotExist:
+        e = None
+
+    if u is not None:
+        context = {'error_message': 'User Exists'}
+        return render(request, 'messenger/index.html', context)
+
+    if e is not None:
+        context = {'error_message': 'Email Exists'}
+        return render(request, 'messenger/index.html', context)
+
+    user = User.objects.create_user(first_name=first_name, last_name=last_name,
+                                    username=username, password=password,
+                                    email=email)
+
+    context = {'user_created': 'User Created'}
     return render(request, 'messenger/index.html', context)
 
-def detail(request, id):
-    return HttpResponse(id)
+
+@login_required(login_url='/')
+def webchat(request):
+    return HttpResponse('Chat Page')
+
